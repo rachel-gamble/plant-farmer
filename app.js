765f41591c733bcfae48f576d00b5c7cc4437891
Rachel Gamble <sautrah@gmail.com>
2022-11-04 16:55:43 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+let = 0
+let autoGold = 0
+let totalGoldMultiplier = 0
+
+console.log('Welcome to Plant Farmer ✨🌱✨')
+
+let plant = {
+
+}
+
+const clickUpgrades = [
+    {
+        name: 'sheers',
+        price: 100,
+        quantity: 0,
+        multiplier: 1
+    }, {
+        name: 'fertilizer',
+        price: 500,
+        quantity: 0,
+        multiplier: 5
+    }
+]
+
+let automaticUpgrades = [{
+    name: 'flowers',
+    price: 600,
+    quantity: 0,
+    multiplier: 10
+}, {
+    name: 'fruit',
+    price: 1000,
+    quantity: 0,
+    multiplier: 20,
+}
+]
+
+// function water{
+
+// }
+
+// function update{
+
+// }
+
+// function buySheers {
+
+// }
+
+// function:
+
